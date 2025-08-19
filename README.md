35b09c6b2640c5126243fc839a59121b4a163473
calvoclucas <calvoclucas@gmail.com>
2025-08-19 20:44:51 -0300

    Clase 2 TypeScript

--- README.md
+++
@@ -1,2 +0,0 @@
-# app-mdw-2025
-Aplicación de Metodologías y Desarrollo Web 2025 Speranza - Calvo - Lozano - Guerra
---
+++ src/types.ts
@@ -0,0 +1,50 @@
+//* TIPADOS DE TYPESCRIPT *//
+const x: number = 30;
+const y: number = 30;
+
+const res = x + y;
+
+type ning = string & number;
+
+type Person = {
+  age: number;
+  name: string;
+  birthDate: Date;
+  isMarried?: boolean;
+} | null;
+
+type PersonExtended =
+  | (Person & {
+      lastName: string;
+    })
+  | null;
+
+const personSample: Person = {
+  age: 21,
+  name: "Jhon",
+  birthDate: new Date("1992-07-22"),
+};
+
+interface PersonInterface {
+  age: number;
+  name: string;
+  birthDate: Date;
+  isMarried?: boolean;
+}
+
+interface PersonInterfaceExtended extends PersonInterface {
+  lastName: string;
+}
+
+type PersonPartial = Partial<Person>;
+
+const personIntfSample: PersonInterfaceExtended = {
+  age: 21,
+  name: "Jhon",
+  birthDate: new Date("1992-07-22"),
+  lastName: "Doe",
+};
+
+interface PersonInterfaceShortened extends Omit<PersonInterface, "birthDate"> {
+  lastName: string;
+}
