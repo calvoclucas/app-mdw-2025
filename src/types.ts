bfdd8ac4ebc1bc9cd397d6633fdbc4deb359bb40
calvoclucas <calvoclucas@gmail.com>
2025-08-19 21:10:12 -0300

    TypeScript Clase 2 - Funciones

--- src/types.ts
+++ src/types.ts
@@ -64,3 +64,23 @@ const clientSample: Client = {
   password: "1234",
   role: UserRole.ADMIN,
 };
+
+interface args {
+  arg1: string;
+  arg2: number;
+}
+type MyFunction = (arg1: string, arg2: number) => string;
+
+const functionSample: MyFunction = (arg1: string, arg2: number) => {
+  return arg1 + arg2;
+};
+
+type MyFunction1 = (arg: args) => string;
+
+const functionSample1: MyFunction1 = (args: args) => {
+  const { arg1, arg2 } = args; //Destructuring
+  return arg1 + arg2;
+};
+
+const NOSEHACE: any = new Date("12");
+const NOSEHACE1: unknown = new Date("12");
