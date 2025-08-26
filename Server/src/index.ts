07a4d380c7a86cbd97cc0e3d6c7757891fc6af21
calvoclucas <calvoclucas@gmail.com>
2025-08-26 20:48:46 -0300

    Clase 3

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./db";
 
 dotenv.config();
+connectDB();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
