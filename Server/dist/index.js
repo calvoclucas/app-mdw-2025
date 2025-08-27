002f051edda703735702ee4e29564bf481186244
calvoclucas <calvoclucas@gmail.com>
2025-08-26 21:37:50 -0300

    Clase 3

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const db_1 = __importDefault(require("./db"));
 dotenv_1.default.config();
+(0, db_1.default)();
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
