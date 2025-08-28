42d37c2d2474b21ec98bae56b098762ef0c5c266
calvoclucas <calvoclucas@gmail.com>
2025-08-28 12:56:06 -0300

    Por el momento genero el post users dentro del index.ts la proxima lo hacemos dentro de Controlladores y en routes con su ruta correspondiente

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./db"));
+const User_1 = __importDefault(require("./models/User"));
 dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
@@ -15,6 +16,18 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Server is ON");
+});
+app.post("/users", async (req, res) => {
+    console.log("Body recibido:", req.body); // <-- agregalo
+    try {
+        const user = new User_1.default(req.body);
+        const saved = await user.save();
+        res.status(201).json(saved);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(400).json({ error });
+    }
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db";
+import User from "./models/User";
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,18 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is ON");
+});
+
+app.post("/users", async (req, res) => {
+  console.log("Body recibido:", req.body);
+  try {
+    const user = new User(req.body);
+    const saved = await user.save();
+    res.status(201).json(saved);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error });
+  }
 });
 
 app.listen(PORT, () => {
