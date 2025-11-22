c53af0c343f5db0c8961018251a57468df7f30f4
calvoclucas <calvoclucas@gmail.com>
2025-11-22 12:25:24 -0300

    Agrego Variables de Entorno

--- altoque/src/firebase.ts
+++ altoque/src/firebase.ts
@@ -1,19 +1,18 @@
-// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoVgAOs6x1NgQxycfZNS4MtsyHKJ7TUwU",
-  authDomain: "altoque-74941.firebaseapp.com",
-  projectId: "altoque-74941",
-  storageBucket: "altoque-74941.firebasestorage.app",
-  messagingSenderId: "956788600883",
-  appId: "1:956788600883:web:4997a86fe70500055d7346",
-  measurementId: "G-SH4JSYT2H3",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // <-- Exportar auth
-export const analytics = getAnalytics(app); // <-- opcional
+
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
